14d6411f3b2c06aa1c58014e8e52498b6135e894
MrShadyDuck <112652645+MrShadyDuck@users.noreply.github.com>
2022-12-23 12:22:02 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
-if game.Id == 8425637426 then
+if place.Id == 8425637426 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrShadyDuck/Birb-s-Hub/main/BirbHub.lua"))()
 else
     game.Players.LocalPlayer:Kick("Game Unsupported.")
